ca67fd5ade8ff8944339b007d9d25e72a5cc5a75
Deva Prasad <devaprasad.ash@gmail.com>
2026-03-27 11:50:26 +0530

    Fix RetrievalQA import for Streamlit Cloud

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import FAISS
-from langchain.chains import RetrievalQA
+from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain_core.prompts import PromptTemplate
 
 # =============================
